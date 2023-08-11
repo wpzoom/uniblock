ab04d9a034579ae59daa6f452ce7c255a272d201
Alexei Samarschi <samarschi@gmail.com>
2023-08-11 13:27:55 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -336,7 +336,7 @@ function uniblock_get_fonts_url( $all = false ) {
     );
 
 	//Get user's saved custom typography
-	$user_custom_typos = uniblock_theme_get_custom_typography();
+	$user_custom_typo = uniblock_theme_get_custom_typography();
 	
 	//Combine default and custom typography
 	$theme_custom_typo = array_merge( $user_custom_typo, $theme_default_typo );
